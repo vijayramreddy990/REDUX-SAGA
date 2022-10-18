ffc03fe2b852d3594cb478572180e72c544034b6
vijay <vijayramreddy990@gmail.com>
2022-10-18 23:56:31 +0530

    form

--- src/pages/AddEditUser.js
+++ src/pages/AddEditUser.js
@@ -1,10 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
+import { MDBValidation, MDBInput, MDBBtn } from "mdb-react-ui-kit";
+import { useNavigate } from "react-router-dom";
+
+const initialState = {
+  name: "",
+  email: "",
+  phone: "",
+  address: "",
+};
 
 const AddEditUser = () => {
+  const [formValue, setFormValue] = useState(initialState);
+  const { name, email, phone, address } = formValue;
+
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {};
+
+  const onInputChange = (e) => {
+    let { name, value } = e.target;
+    setFormValue({
+      ...formValue,
+      [name]: value,
+    });
+  };
+
   return (
-    <div>
-      <h2>AddEditUser</h2>
-    </div>
+    <MDBValidation
+      className="row g-3"
+      style={{ marginTop: "100px" }}
+      onSubmit={handleSubmit}
+    >
+      <p className="fs-2 fw-bold">Add User Detail</p>
+      <div
+        style={{
+          margin: "auto",
+          padding: "15px",
+          maxWidth: "400px",
+          alignContent: "center",
+        }}
+      >
+        <MDBInput
+          value={name}
+          name="name"
+          type="text"
+          onChange={onInputChange}
+          required
+          label="Name"
+          validation="Please provide a name"
+          invalid
+        />
+        <br />
+        <MDBInput
+          value={email}
+          name="email"
+          type="email"
+          onChange={onInputChange}
+          required
+          label="Email"
+          validation="Please provide a email"
+          invalid
+        />
+        <br />
+        <MDBInput
+          value={phone}
+          name="phone"
+          type={"number"}
+          onChange={onInputChange}
+          required
+          label="Phone"
+          validation="Please provide a Phone no."
+          invalid
+        />
+        <br />
+        <MDBInput
+          value={address}
+          name="address"
+          type={"text"}
+          onChange={onInputChange}
+          required
+          label="Address"
+          validation="Please provide an Address"
+          invalid
+        />
+      </div>
+      <div className="col-12">
+        <MDBBtn style={{ marginRight: "10px" }} type="submit">
+          Add
+        </MDBBtn>
+        <MDBBtn onClick={() => navigate("/")} color="danger">
+          Go Back
+        </MDBBtn>
+      </div>
+    </MDBValidation>
   );
 };
 
