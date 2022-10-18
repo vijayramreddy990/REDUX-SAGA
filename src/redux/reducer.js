c3168b89feb3e89b22f2189b1246690d0cbb73a7
vijay <vijayramreddy990@gmail.com>
2022-10-18 21:27:14 +0530

    setting reducers and action types

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,10 +1,30 @@
+import * as types from "./actionTypes";
+
 const initialState = {
   users: [],
   loading: false,
+  error: null,
 };
 
 const usersReducer = (state = initialState, action) => {
   switch (action.type) {
+    case types.LOAD_USERS_START:
+      return {
+        ...state,
+        loading: true,
+      };
+    case types.LOAD_USERS_SUCCESS:
+      return {
+        ...state,
+        users: action.payload,
+        loading: false,
+      };
+    case types.LOAD_USERS_ERROR:
+      return {
+        ...state,
+        error: action.payload,
+        loading: false,
+      };
     default:
       return state;
   }
