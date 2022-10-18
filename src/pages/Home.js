5c9626c428e76e0279c6793dd6c8e0319178884e
vijay <vijayramreddy990@gmail.com>
2022-10-18 22:39:16 +0530

    fetching the api data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { loadUsersStart } from "../redux/actions";
 
 const Home = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadUsersStart());
+  }, []);
+
   return (
     <div>
       <h2>Home</h2>
--- src/redux/usersaga.js
+++ src/redux/usersaga.js
@@ -13,8 +13,19 @@ import { loadUsersSuccess, loadUsersError } from "./actions";
 import * as types from "./actionTypes";
 import { loadUsersApi } from "./api";
 
-export function* onLoadUsersStartAsync() {}
+export function* onLoadUsersStartAsync() {
+  try {
+    const response = yield call(loadUsersApi);
+    if (response.status === 200) {
+      yield delay(500);
+      yield put(loadUsersSuccess(response.data));
+    }
+  } catch (error) {
+    yield put(loadUsersError(error.response.data));
+  }
+}
 
+//when ever this action tipe is matched it will fire the respective handler
 export function* onLoadUsers() {
   yield takeEvery(types.LOAD_USERS_START, onLoadUsersStartAsync);
 }
