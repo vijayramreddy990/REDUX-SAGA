f38a1b1f6310759acc89f558a306b7d217b1133d
vijay <vijayramreddy990@gmail.com>
2022-10-18 23:13:52 +0530

    rendering home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,90 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { loadUsersStart } from "../redux/actions";
+import {
+  MDBTable,
+  MDBTableHead,
+  MDBTableBody,
+  MDBBtn,
+  MDBIcon,
+  MDBTooltip,
+  MDBSpinner,
+} from "mdb-react-ui-kit";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   const dispatch = useDispatch();
+  const { users } = useSelector((state) => state.data);
 
   useEffect(() => {
     dispatch(loadUsersStart());
   }, []);
 
+  const handleDelete = (id) => {};
+
   return (
-    <div>
-      <h2>Home</h2>
+    <div className="container" style={{ marginTop: "150px" }}>
+      <MDBTable>
+        <MDBTableHead dark>
+          <tr>
+            <th scope="col">No.</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Phone</th>
+            <th scope="col">Address</th>
+            <th scope="col">Action</th>
+          </tr>
+        </MDBTableHead>
+        {users &&
+          users.map((item, index) => (
+            <MDBTableBody key={index}>
+              <tr>
+                <th scope="row">{index + 1}</th>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td>{item.phone}</td>
+                <td>{item.address}</td>
+                <td>
+                  <MDBBtn
+                    className="m-1"
+                    tag={"a"}
+                    color="none"
+                    onClick={() => handleDelete(item.id)}
+                  >
+                    <MDBTooltip title="Delete" tag={"a"}>
+                      <MDBIcon
+                        fas
+                        icon="trash"
+                        style={{ color: "#dd4b39" }}
+                        size="lg"
+                      />
+                    </MDBTooltip>
+                  </MDBBtn>{" "}
+                  <Link to={`editUser/${item.id}`}>
+                    <MDBTooltip title="Edit" tag={"a"}>
+                      <MDBIcon
+                        fas
+                        icon="pen"
+                        style={{ color: "#55acee", marginBottom: "10px" }}
+                        size="lg"
+                      />
+                    </MDBTooltip>
+                  </Link>{" "}
+                  <Link to={`userInfo/${item.id}`}>
+                    <MDBTooltip title="View" tag={"a"}>
+                      <MDBIcon
+                        fas
+                        icon="eye"
+                        style={{ color: "#000000", marginBottom: "10px" }}
+                        size="lg"
+                      />
+                    </MDBTooltip>
+                  </Link>
+                </td>
+              </tr>
+            </MDBTableBody>
+          ))}
+      </MDBTable>
     </div>
   );
 };
