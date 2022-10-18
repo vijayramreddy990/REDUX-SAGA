1a9c81dd1c6f1b54c2758ca5534ad0bb27f22b38
vijay <vijayramreddy990@gmail.com>
2022-10-18 19:31:25 +0530

    router setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import AddEditUser from "./pages/AddEditUser";
+import UserInfo from "./pages/UserInfo";
+import About from "./pages/About";
 
 const App = () => {
   return (
-    <div>
-      <p className="fw-bold">
-        Heloo World! This is a reduc crud application with react
-      </p>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/addUser" element={<AddEditUser />} />
+          <Route path="/editUser/:id" element={<AddEditUser />} />
+          <Route path="/userInfo/:id" element={<UserInfo />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 };
 
